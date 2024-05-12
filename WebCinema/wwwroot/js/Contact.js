6ec9b73cb3045ed77d7c47215d4902f79c3a8dd4
CHAUKIENLUONG <luong211003@gmail.com>
2024-05-12 23:32:29 +0700

    update js of contact.html

--- WebCinema/wwwroot/js/Contact.js
+++ WebCinema/wwwroot/js/Contact.js
@@ -1,6 +1,4 @@
-﻿// your_script.js
-
-(function () {
+﻿(function () {
     emailjs.init("NKaEqYATtE-LtFLAr"); // Replace 'user_youruserid' with your user ID from EmailJS
 
     document.getElementById('contact-form').addEventListener('submit', function (event) {
@@ -9,11 +7,13 @@
         const formData = {
             name: this.name.value,
             email: this.email.value,
-            message: this.message.value
+            message: this.message.value,
+            from_name: this.name.value,
+            from_mail: this.email.value,
         };
 
         // Change 'your_service_id', 'your_template_id', and 'user_youruserid' with your own values
-        emailjs.send('service_fn650ah', 'template_1i3mtov', formData)
+        emailjs.send('service_r1s888n', 'template_8im6bag', formData)
             .then(function (response) {
                 console.log('Success!', response.status, response.text);
                 alert('Your message has been sent successfully!');
