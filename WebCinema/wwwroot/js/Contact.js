986dc4c673815ea0a47f5cbaa25130daa7b7d0d5
CHAUKIENLUONG <luong211003@gmail.com>
2024-05-22 09:27:01 +0700

    change contact

--- WebCinema/wwwroot/js/Contact.js
+++ WebCinema/wwwroot/js/Contact.js
@@ -1,25 +1,42 @@
-﻿(function () {
-    emailjs.init("NKaEqYATtE-LtFLAr"); // Replace 'user_youruserid' with your user ID from EmailJS
+﻿document.addEventListener("DOMContentLoaded", () => {
+    const name = document.getElementById("name");
+    const email = document.getElementById("email");
+    const message = document.getElementById("message");
+    const submit = document.getElementById("submit");
 
-    document.getElementById('contact-form').addEventListener('submit', function (event) {
-        event.preventDefault();
-        // this is the ID of your form
-        const formData = {
-            name: this.name.value,
-            email: this.email.value,
-            message: this.message.value,
-            from_name: this.name.value,
-            from_mail: this.email.value,
+    submit.addEventListener("click", (e) => {
+        e.preventDefault(); // Ensure default form submission is prevented
+        const data = {
+            name: name.value,
+            email: email.value,
+            message: message.value,
         };
-
-        // Change 'your_service_id', 'your_template_id', and 'user_youruserid' with your own values
-        emailjs.send('service_r1s888n', 'template_8im6bag', formData)
-            .then(function (response) {
-                console.log('Success!', response.status, response.text);
-                alert('Your message has been sent successfully!');
-            }, function (error) {
-                console.error('Failed...', error);
-                alert('Oops! Something went wrong. Please try again later.');
-            });
+        if (!data.name || !data.email || !data.message) {
+            alert("Vui lòng điền đầy đủ thông tin.");
+        } else if (!data.email.includes('@')) {
+            alert("Phải là địa chỉ Email!");
+        } else {
+            postGoogle(data);
+        }
     });
-})();
+
+    async function postGoogle(data) {
+        const formURL = "https://docs.google.com/forms/d/e/1FAIpQLScPw_wcIqIfz7gqf6ULil8VD9yPft8N573hSK_R3c69uN8cDA/formResponse";
+        const formData = new FormData();
+        formData.append("entry.1094708025", data.name);
+        formData.append("entry.706364320", data.email);
+        formData.append("entry.228466318", data.message);
+
+        try {
+            await fetch(formURL, {
+                method: "POST",
+                body: formData,
+                mode: "no-cors"
+            });
+            alert("Thông tin liên hệ đã được gửi.");
+        } catch (error) {
+            alert("Có lỗi xảy ra khi gửi thông tin.");
+            console.error("Error:", error);
+        }
+    }
+});
