c27e1bbb0e2a63d0885b294f2878972494ed45e2
CHAUKIENLUONG <luong211003@gmail.com>
2024-05-27 01:45:30 +0700

    update html css js (layout customer trailer)

---
+++ WebCinema/wwwroot/js/Trailer.js
@@ -0,0 +1,34 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    var modal = document.getElementById('trailerModal');
+    var iframe = document.getElementById('trailerIframe');
+    var span = document.getElementsByClassName('close')[0];
+
+    document.querySelectorAll('.view-trailer').forEach(function (button) {
+        button.addEventListener('click', function (event) {
+            event.preventDefault();  // Ngăn chặn hành động mặc định của thẻ a
+            var trailer = this.getAttribute('data-trailer');
+            // Kiểm tra và chuyển đổi URL thành định dạng nhúng nếu cần
+            if (trailer.startsWith("https://www.youtube.com/watch?v=")) {
+                let videoId = trailer.split("v=")[1];
+                trailer = "https://www.youtube.com/embed/" + videoId;
+            }
+            else if (!trailer.startsWith("https://www.youtube.com/embed/")) {
+                trailer = "https://www.youtube.com/embed/" + trailer;
+            }
+            iframe.src = trailer;
+            modal.style.display = 'block';
+        });
+    });
+
+    span.onclick = function () {
+        modal.style.display = 'none';
+        iframe.src = '';
+    };
+
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = 'none';
+            iframe.src = '';
+        }
+    };
+});
